4c9331fb012e9c48efc08ba969176bcf79c7f1a6
Medhunraj <104486882+Medhunraj@users.noreply.github.com>
2022-12-28 22:50:05 +0530

    Create SumDiff.c

---
+++ SumDiff.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int a,b;
+    float c,d;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%f",&c);
+    scanf("%f",&d);
+	
+    printf("%d %d\n%.1f %.1f",a+b,a-b,c+d,c-d);
+    return 0;
+}
